d4561f150f14d79989a90c87686b8f01adba1f2e
Jake Mu <jakemu6@gmail.com>
2026-03-05 14:23:09 -0500

    Init commit

---
+++ projects/Fishpond/Source/Fishpond/Fishpond.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Fishpond.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fishpond, "Fishpond" );
